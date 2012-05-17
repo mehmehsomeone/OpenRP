69920d066646025d6afc81bb88314783f8ee1c7a
Fighter298 <Fighter298xCoDx@gmail.com>
2012-05-17 23:49:36 +0000

    Updated MakePK3.bat.
    Updated 7za.exe to the latest version.
    
    git-svn-id: http://openrp.googlecode.com/svn/trunk@101 3c299bc4-26c0-bb6f-2937-d8014291f781

--- MakePK3.bat
+++ MakePK3.bat
@@ -5,7 +5,7 @@ rem VARIABLE DEFINES
 rem ****************
 
 set PK3DLL=OpenRP
-set PK3ASSETS=OpenRP_stuff
+set PK3CONTENT=OpenRP_Content
 set ASSETSFOLDER=OpenRP
 
 rem ***************
@@ -14,12 +14,12 @@ rem ***************
 
 ECHO.
 ECHO ===========
-ECHO Making Pk3s
+ECHO Making Pk3s 
 ECHO ===========
 
-ECHO Y | DEL %PK3DLL%
-ECHO Y | DEL %PK3ASSETS%
-7za.exe a -tzip %ASSETSFOLDER%\%PK3ASSETS%.pk3 .\%ASSETSFOLDER%\* -xr!.screenshots\ -xr!.svn\ -x!*.dll -x!*.so -x!.\%ASSETSFOLDER%\*.* -xr!*.nav -mx9
+ECHO Y | DEL %PK3DLL%.pk3
+ECHO Y | DEL %PK3CONTENT%.pk3
+7za.exe a -tzip %ASSETSFOLDER%\%PK3CONTENT%.pk3 .\%ASSETSFOLDER%\* -mx9
 IF ERRORLEVEL 1 GOTO ERROR
 7za.exe a -tzip %ASSETSFOLDER%\%PK3DLL%.pk3 .\%ASSETSFOLDER%\*.dll -mx9
 
@@ -39,5 +39,3 @@ GOTO END
 
 
 :END
-
-
